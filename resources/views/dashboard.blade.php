ee1ffde7171572393b0e2528031070ba4f1fd9aa
jimdev90 <jimpv100190@gmail.com>
2023-11-24 20:48:49 -0500

    view unidades con logros y sin logros en el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,42 +15,48 @@
                                     <h4 class="text-center">{{ auth()->user()->nombre }}</h4>
                                 </div>
                                 <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="alert alert-success text-center">
-                                                <h3>Unidades con logros el día de hoy</h3>
+                                    @if(auth()->user()->unidad_usuario->profile === '99')
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="alert alert-success text-center">
+                                                    <h3>Unidades con logros el día de hoy</h3>
+                                                </div>
+                                                <div class="list-group">
+                                                    @php
+                                                        $i = 1;
+                                                    @endphp
+                                                    @forelse($unidadesConLogros as $ucl)
+                                                        <a href="#" class="list-group-item list-group-item-action">
+                                                            {{ $i++ }} .- {{ $ucl->unidad->name }}
+                                                        </a>
+                                                    @empty
+                                                        <p class="text-center fw-bold">
+                                                            <stron>Sin registros</stron>
+                                                        </p>
+                                                    @endforelse
+                                                </div>
                                             </div>
-                                            <div class="list-group">
-                                                @php
-                                                    $i = 1;
-                                                @endphp
-                                                @forelse($unidadesConLogros as $ucl)
-                                                    <a href="#" class="list-group-item list-group-item-action">
-                                                        {{ $i++ }} .- {{ $ucl->unidad->name }}
-                                                    </a>
-                                                @empty
-                                                    <p class="text-center fw-bold"><stron>Sin registros</stron></p>
-                                                @endforelse
+                                            <div class="col-6">
+                                                <div class="alert alert-danger text-center">
+                                                    <h3>Unidades sin logros el día de hoy</h3>
+                                                </div>
+                                                <div class="list-group">
+                                                    @php
+                                                        $i = 1;
+                                                    @endphp
+                                                    @forelse($unidadesSinLogros as $usl)
+                                                        <a href="#" class="list-group-item list-group-item-action">
+                                                            {{ $i++ }} .- {{ $usl->unidad->name }}
+                                                        </a>
+                                                    @empty
+                                                        <p class="text-center fw-bold">
+                                                            <stron>Sin registros</stron>
+                                                        </p>
+                                                    @endforelse
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="col-6">
-                                            <div class="alert alert-danger text-center">
-                                                <h3>Unidades sin logros el día de hoy</h3>
-                                            </div>
-                                            <div class="list-group">
-                                                @php
-                                                    $i = 1;
-                                                @endphp
-                                                @forelse($unidadesSinLogros as $usl)
-                                                    <a href="#" class="list-group-item list-group-item-action">
-                                                        {{ $i++ }} .- {{ $usl->unidad->name }}
-                                                    </a>
-                                                @empty
-                                                    <p class="text-center fw-bold"><stron>Sin registros</stron></p>
-                                                @endforelse
-                                            </div>
-                                        </div>
-                                    </div>
+                                    @endif
                                 </div>
                             </div>
                         </div>
